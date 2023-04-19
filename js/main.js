249abefcc33b7f2fca4d27cfec148d9ba67b9402
roma4ka0 <111571102+roma4ka0@users.noreply.github.com>
2023-04-19 14:36:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var Application = new Vue({
           title: "Lemon1",
           text: "Лемон",
           image: "Lemon2.jfif",
-          desc: "Full desc",
+          desc: "Full desc1",
         },
   
         {
@@ -15,7 +15,7 @@ var Application = new Vue({
           title: "Lemon2",
           text: "Лемон",
           image: "Lemon3.jfif",
-          desc: "Full desc",
+          desc: "Full desc2",
         },
   
         {
@@ -23,7 +23,7 @@ var Application = new Vue({
           title: "Lemon3",
           text: "Лемон",
           image: "Lemon4.jfif",
-          desc: "Full desc",
+          desc: "Full desc3",
         },
   
         {
@@ -31,7 +31,7 @@ var Application = new Vue({
           title: "Lemon4",
           text: "Лемон",
           image: "Lemon5.jfif",
-          desc: "Full desc",
+          desc: "Full desc4",
         },
   
         {
@@ -39,16 +39,93 @@ var Application = new Vue({
           title: "Lemon5",
           text: "Лемон",
           image: "Lemon7.jfif",
-          desc: "Full desc",
+          desc: "Full desc5",
         },
       ],
+
+      product: [],
+      lemn: [],
+      contactFields: [],
+      btnVisible: 0,
+      order: 0,
     },
     mounted: function () {
-      console.log(window.localStorage.getItem("Product"));
+      this.getProduct();
+      this.checkInLemn();
+      this.getLemn();
     },
     methods: {
-      addItem: function (id) {
-        window.localStorage.setItem("Product", id);
+      getProduct() {
+        if (window.location.hash) {
+          var id = window.location.hash.replace("#", "");
+          if (this.products && this.products.length > 0) {
+            for (i in this.products) {
+              if (
+                this.products[i] &&
+                this.products[i].id &&
+                id == this.products[i].id
+              )
+                this.product = this.products[i];
+            }
+          }
+        }
+      },
+      addToLemn(id) {
+        var lemn = [];
+        if (window.localStorage.getItem("lemn")) {
+          lemn = window.localStorage.getItem("lemn").split(",");
+        }
+  
+        if (lemn.indexOf(String(id)) == -1) {
+          lemn.push(id);
+          window.localStorage.setItem("lemn", lemn.join());
+          this.btnVisible = 1;
+        }
+      },
+      checkInLemn() {
+        if (
+          this.product &&
+          this.product.id &&
+          window.localStorage
+            .getItem("lemn")
+            .split(",")
+            .indexOf(String(this.product.id)) != -1
+        )
+          this.btnVisible = 1;
+      },
+      getLemn() {
+        if (window.localStorage.getItem("lemn") != null) {
+          if (this.products != null && this.products.length > 0) {
+            for (let i in this.products) {
+              if (
+                this.products[i] != null &&
+                this.products[i].id != null &&
+                window.localStorage
+                  .getItem("lemn")
+                  .split(",")
+                  .indexOf(String(this.products[i].id)) != -1
+              )
+                this.lemn.push(this.products[i]);
+            }
+          }
+        }
+      },
+      removeFromLemn(id) {
+        let lemn = [];
+        if (window.localStorage.getItem("lemn") != null) {
+          lemn = window.localStorage.getItem("lemn").split(",");
+        }
+        if (lemn.indexOf(String(id)) != -1) {
+          lemn.splice(lemn.indexOf(String(id)), 1);
+          window.localStorage.setItem("lemn", lemn.join(","));
+          this.lemn = [];
+          this.getLemn();
+        }
+      },
+      makeOrder() {
+        this.lemn = [];
+        window.localStorage.setItem("lemn", "");
+        this.order = 1;
       },
     },
   });
